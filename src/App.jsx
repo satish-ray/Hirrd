983675786bf77b6ad7447f51d2662200234222be
Satish Kumar Ray <www.satishray@gmail.com>
2025-10-20 19:10:32 +0545

    project setup
    Signed-off-by: Satish Kumar Ray <www.satishray@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {Button} from "@/components/ui/button"
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+     
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default App
